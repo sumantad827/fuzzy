25c9863cc770c98b1e11d80cc414ab41b190603a
sumantad827 <sumanta.das@weavers-web.com>
2025-09-17 14:42:26 +0530

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This is my third commit one of his
       </p>
     </>
   )
